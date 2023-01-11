0ed5cec5c7c89acc6c02723141c83bf72d60c000
SavePlanetDev <116899574+SavePlanetDev@users.noreply.github.com>
2023-01-11 13:08:23 +0000

    add title

--- components/header.js
+++ components/header.js
@@ -4,6 +4,7 @@ import styles from "../styles/Header.module.css";
 export default function Header() {
   return (
     <div>
+      <title>Chicken-DAO</title>
       <div className={styles.logo}>
         <div>
           <img src="logochic.png" className={styles.logoimg}></img>
